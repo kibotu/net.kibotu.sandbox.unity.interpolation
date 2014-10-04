449a3d89771591d6eb10ed218ae79a7fcb43929e
Jan Rabe <jan.rabe@wooga.net>
2014-10-04 22:03:40 +0200

    added timer for particle end

--- Assets/Sources/Sequence.cs
+++ Assets/Sources/Sequence.cs
@@ -6,38 +6,48 @@ namespace Assets.Sources
 {
     public class Sequence : MonoBehaviour
     {
-        public MoveAlongPath[] Path;
-        public Interpolate.EaseType EaseType = Interpolate.EaseType.EaseInOutSine;
-        public Transform [] ControlPoints;
-        public float Duration;
-        public int Slices;
-        private IEnumerable<Vector3> _sequence;
         public enum Type
         {
-            Line, Bezier, CatmullRom, ParticleCatMullRom
+            Line,
+            Bezier,
+            CatmullRom,
+            ParticleCatMullRom
         }
 
-        private bool _isRunning;
+        public Transform[] ControlPoints;
+        public float Duration;
+        public Interpolate.EaseType EaseType = Interpolate.EaseType.EaseInOutSine;
         public ParticleSystem Particles;
+        public MoveAlongPath[] Path;
+        public int Slices;
+
+        private bool _isRunning;
+        private IEnumerable<Vector3> _sequence;
 
         public void StartCube(int type)
         {
-            if (_isRunning) 
+            if (_isRunning)
                 return;
 
             _isRunning = true;
             switch (type)
             {
-                case (int)Type.Line:
-                    Path[0].LoopMoving().OnFulfilled += result => Path[1].LoopMoving().OnFulfilled += result2 => Path[2].LoopMoving().OnFulfilled += result3 => Path[3].LoopMoving().OnFulfilled += vector3 => _isRunning = false;
+                case (int) Type.Line:
+                    Path[0].LoopMoving().OnFulfilled +=
+                        result =>
+                            Path[1].LoopMoving().OnFulfilled +=
+                                result2 =>
+                                    Path[2].LoopMoving().OnFulfilled +=
+                                        result3 => Path[3].LoopMoving().OnFulfilled += vector3 => _isRunning = false;
                     break;
-                case (int)Type.Bezier:
+                case (int) Type.Bezier:
                     StartCoroutine(MoveBezier());
                     break;
-                case (int)Type.CatmullRom:
+                case (int) Type.CatmullRom:
                     StartCoroutine(MoveCatmullRom());
                     break;
-                case (int)Type.ParticleCatMullRom:
+                case (int) Type.ParticleCatMullRom:
+                    StartCoroutine(Timer.WaitUntilInterval(Particles.duration, () => _isRunning = false));
                     Particles.Play();
                     break;
             }
@@ -47,7 +57,8 @@ namespace Assets.Sources
         {
             yield return new WaitForSeconds(1);
             IEnumerable<Vector3> sequence = Interpolate.NewBezier(Interpolate.Ease(EaseType), ControlPoints, Duration);
-            foreach (Vector3 newPoint in sequence) {
+            foreach (Vector3 newPoint in sequence)
+            {
                 transform.position = newPoint;
                 yield return new WaitForEndOfFrame();
             }
@@ -58,7 +69,8 @@ namespace Assets.Sources
         {
             yield return new WaitForSeconds(1);
             IEnumerable<Vector3> sequence = Interpolate.NewCatmullRom(ControlPoints, Slices, false);
-            foreach (Vector3 newPoint in sequence) {
+            foreach (Vector3 newPoint in sequence)
+            {
                 transform.position = newPoint;
                 yield return new WaitForEndOfFrame();
             }
---
+++ Assets/Sources/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Sources
+{
+    public class Timer
+    {
+        public float Interval;
+        public bool Debug = false;
+        public event Action OnComplete;
+
+        public IEnumerable<IEnumerator> Start()
+        {
+            yield return WaitUntilInterval();
+        }
+
+        public IEnumerator WaitUntilInterval() {
+            yield return new WaitForSeconds(Interval);
+            OnComplete();
+        }
+
+        public static IEnumerator WaitUntilInterval(float duration, Action onComplete)
+        {
+            yield return new WaitForSeconds(duration);
+            onComplete();
+        }
+    }
+}
